b7b1bda2ea835c6a36c336a2fa0b1a972ed52d25
Stephany Pena <stephany.pena@mail.citytech.cuny.edu>
2020-10-19 16:57:52 -0400

    added cake.py

---
+++ cake.py
@@ -0,0 +1,7 @@
+userAnswer = input("Do you want some cake? ")
+if userAnswer == "yes" or userAnswer == "YES":
+    print("Have some cake.")
+elif userAnswer == "no" or userAnswer == "NO": 
+    print("No cake for you.")
+else:
+    print("I don't understand.")

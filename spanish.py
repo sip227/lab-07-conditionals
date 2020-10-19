da673748b4746b9611146e5e1c7ebeb8dbd3000d
Stephany Pena <stephany.pena@mail.citytech.cuny.edu>
2020-10-19 17:07:01 -0400

    added spanish.py

---
+++ spanish.py
@@ -0,0 +1,13 @@
+
+userAnswer = input("Give me a word in English... ")
+if userAnswer == "cat" or userAnswer == "CAT":
+    print("gato")
+elif userAnswer == "dog" or userAnswer == "DOG":
+    print("perro")
+elif userAnswer == "horse" or userAnswer == "HORSE":
+    print("caballo")  
+else:
+    print("no entiendo")
+
+            
+            
